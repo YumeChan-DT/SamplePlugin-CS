99a54a64c2de782dbf533f0295d2ee2d452f7c34
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2022-04-25 13:43:22 +0200

    Update to YumeChan 3 standards
    This sample desperately needs some love... <3

--- PluginManifest.cs
+++ PluginManifest.cs
@@ -1,32 +1,40 @@
-﻿using System;
-using System.Threading.Tasks;
-using Nodsoft.YumeChan.PluginBase;
+﻿using YumeChan.PluginBase;
 
-namespace Nodsoft.YumeChan.PluginSampleCS
+namespace YumeChan.PluginSampleCS
 {
 	// This Class defines the Plugin, and makes it visible to the Loader.
+	
+	/// <summary>
+	/// Plugin Manifest for PluginSampleCS.
+	/// </summary>
 	public class PluginManifest : Plugin // This Class MUST be set as public to get picked up by the Plugin Loader.
 	{
 		// This defines your Plugin's Display name.
-		public override string PluginDisplayName { get; } = "YumeChan : Sample Plugin";
+		/// <summary>
+		/// Display Name of the PluginSampleCS plugin.
+		/// </summary>
+		public override string DisplayName => "YumeChan : Sample Plugin";
 
 		// This flag defines whether your Plugin should be shown to the general public or not.
 		// Still shown to Server Operators, this is useful for security plugins, or plugins requiring covert operation.
-		public override bool PluginStealth { get; } = false;
+		/// <summary>
+		/// 
+		/// </summary>
+		public override bool StealthMode => false;
 
-		public override async Task LoadPlugin()
+		public override async Task LoadAsync()
 		{
 			//Here goes the Loading Logic, if some loading procedure is needed. Treat it as a Ctor.
 
 
-			await base.LoadPlugin(); // This method call sets Plugin.PluginLoaded to true.
+			await base.LoadAsync(); // This method call sets Plugin.PluginLoaded to true.
 		}
-		public override async Task UnloadPlugin()
+		public override async Task UnloadAsync()
 		{
 			//Here goes the Unloading Logic, if some unloading (e.g: Disposal) is needed. Treat it as a Dtor (~).
 
 
-			await base.UnloadPlugin(); // This method call resets Plugin.PluginLoaded to false.
+			await base.UnloadAsync(); // This method call resets Plugin.PluginLoaded to false.
 		}
 	}
 }
